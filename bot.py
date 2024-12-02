a1cbe8d80e5091c70b0e1c06e69256d67bd6c1b6
anton-kotau <kot98vaz@gmail.com>
2024-12-02 17:50:38 +0100

    add description

--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import logging
 
+#logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
+#function for /start
 def start(update: Update, context: CallbackContext)-> None:
     user = update.effective_user
     user_info = {
@@ -13,14 +15,14 @@ def start(update: Update, context: CallbackContext)-> None:
         "last_name": user.last_name,
         "language_code": user.language_code,
     }
-
+#logging information about user
     logging.info(f"User info:{user_info}")
 
     update.message.reply_text(f"
                               Привет,{user.first_name}! Спасибо, что воспользовались ботом!"
                               )
 
-
+#function for processing any words
 def echo(update: Update, context: CallbackContext)-> None:
     user = update.effective_user
     user_info = {
@@ -33,12 +35,14 @@ def echo(update: Update, context: CallbackContext)-> None:
 
 logging.info(f"User message info: {user_info}")
 
+#answer message
 update.message.reply_text(f"Вы сказали: {update.message.text}")
 
+#main function
 def main():
-    
+    #your token
     updater = Updater("YOUR_TOKEN")
-
+    #registration  processing
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
